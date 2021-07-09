2f1cb82e6befd92383d4e5e5d3e479e25cddc2df
Chase <cabbott93@gmail.com>
2021-07-09 16:25:47 -0700

    failing signup test

---
+++ __tests__/auth.test.js
@@ -0,0 +1,22 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('Auth routes', () => {
+  beforeAll(() => {
+    return setup(pool);
+  });
+  
+  it('signs a user up and stores their info into the database', async () => {
+    const user = {
+      username: 'cabbott93@gmail.com',
+      password: 'password'
+    };
+    const { body } = await request(app)
+      .post('/api/auth/signup')
+      .send(user);
+
+    expect(body).toEqual({ ...user, user_id: 1 });
+  });
+});
