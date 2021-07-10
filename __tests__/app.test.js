aaf79eb93fde491ce62c04fd2ce19bf93fb43e42
Chase Abbott <75389857+chase-abbott@users.noreply.github.com>
2021-07-09 17:48:47 -0700

    Auth (#12)
    * setup cors and heroku db
    
    * created sql tables
    
    * wrote .get route test
    
    * adjusted naming convention to reflect purpose/job
    
    * remove comment
    
    * failing signup test
    
    * Add jwt and bcrypt, passing Auth signup
    
    * add reference keys to .env-example
    
    Co-authored-by: NickDayFSD <Nick.Day.FSD@gmail.com>

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,25 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
+describe.skip('Route tests', () => {
+  beforeAll(() => {
     return setup(pool);
   });
-  it('passes a test', () => {});
+  
+  it('gets a list of couches from craigslist', async () => {
+    // gets a couch list from craigslist
+    const couch = await request(app).get('/api/v1/results/couch');
+
+    // checks to see if we received all first page entries
+    expect(couch.body.length).toBe(120);
+
+    // checks to see if our data comes back in the correct format
+    expect(couch.body[0]).toEqual({
+      title: expect.any(String),
+      image: expect.any(String),
+      price: expect.any(String),
+      link: expect.any(String)
+    });
+  });
+
 });
