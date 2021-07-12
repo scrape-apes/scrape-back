f68d210b3370885292c6990b24528837b61f4490
Chase <cabbott93@gmail.com>
2021-07-12 09:20:28 -0700

    failing login test

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -19,4 +19,27 @@ describe('Auth routes', () => {
 
     expect(body).toEqual({ id: '1', username: 'cabbott93@gmail.com' });
   });
+
+  it('logs in a user that already has an account in the database', async () => {
+    const user = {
+      username: 'cabbott93@gmail.com',
+      password: 'password'
+    };
+
+    await request(app)
+      .post('/api/auth/signup')
+      .send(user);
+
+    const { body } = await request(app)
+      .post('/api/auth/login')
+      .send(user);
+    
+    expect(body).toEqual({ id: '1', username: 'cabbott93@gmail.com' });
+
+    const invalidLoginRes = await request(app)
+      .post('/api/v1/login')
+      .send({ username: 'cabbott93@gmail.com', password: 'pword' });
+
+    expect(invalidLoginRes).toEqual('error');
+  });
 });
