df4497be1d0ae6d93435915b8e610ddbc9a4ae30
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-07-13 10:14:29 -0700

    Verified test works with single word city names

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -3,11 +3,11 @@ import { citySimple } from '../lib/utils/utils.js';
 describe('general utilities', () => {
 
   it('removes spaces from location names', () => {
-    const loc1 = 'New York';
+    const loc1 = 'Portland';
     const loc2 = 'San Francisco';
     const loc3 = 'San Luis Obispo';
 
-    expect(citySimple(loc1)).toEqual('NewYork');
+    expect(citySimple(loc1)).toEqual('Portland');
     expect(citySimple(loc2)).toEqual('SanFrancisco');
     expect(citySimple(loc3)).toEqual('SanLuisObispo');
   });
