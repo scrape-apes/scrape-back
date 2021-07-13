6273ae86a32cd9a9a9ccbe0cc95abfc8e2a8e991
Gabriel Simek <71483160+gabrielsimek@users.noreply.github.com>
2021-07-13 16:06:27 -0700

    Combine results 1 (#20)
    * setup cors and heroku db
    
    * pupeteer heroku config
    
    * Procfile
    
    * profile spellcheck
    
    * node server...
    
    * something
    
    * nodejs buildpack
    
    * heroku config
    
    * Varagesale $ refactor 4 individual scraper modules
    
    * created sql tables
    
    * wrote .get route test
    
    * adjusted naming convention to reflect purpose/job
    
    * remove comment
    
    * failing signup test
    
    * Add jwt and bcrypt, passing Auth signup
    
    * add reference keys to .env-example
    
    * failing login test
    
    * passing login test
    
    * Auth test In Progress - broken tests
    
    * Auth tests - making progress
    
    * remove skip from auth.test.js
    
    * location tested and passes
    
    * tested VS get route
    
    * Varage sale scrape test success
    
    * Scraper route refactor & passing CL & VS tests
    
    * created/ started offerup scraping
    
    * Aggregate branch progress
    
    * Scraper - varage sale refactor to include location
    
    * Scraper - varagesale include location missing file
    
    * virage sale plus craigslist results test passing. getting back 132 items from two separate arrays
    
    * slight modifications
    
    * got prices, munged items with prices
    
    * Offerup - scraper blocked?
    
    * varageSale and craigsList results combined
    
    * offerUp files removed
    
    Co-authored-by: NickDayFSD <Nick.Day.FSD@gmail.com>
    Co-authored-by: CaseyCameron <mcasey.cameron@gmail.com>
    Co-authored-by: Chase <cabbott93@gmail.com>
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>
    Co-authored-by: David Delgadillo <daviddel.731@gmail.com>

---
+++ __tests__/varage-craigslist.test.js
@@ -0,0 +1,37 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe.skip('Route tests', () => {
+  const agent = request.agent(app);
+
+  beforeAll(async () => {
+    await setup(pool);
+    const user = {
+      username: 'cabbott93@gmail.com',
+      password: 'password'
+    };
+
+    await agent
+      .post('/api/auth/signup')
+      .send(user);
+  });
+
+  it('get items off varage sale', async () => {
+    const city = 'Seattle';
+    const searchTerm = 'bike';
+    const items = await agent.get(`/api/v1/results/${searchTerm}/${city}`);
+    expect(items.body.length).toBe(12);
+
+    expect(items.body[0]).toEqual({
+      title: expect.any(String),
+      image: expect.any(String),
+      price: expect.any(String),
+      link: expect.any(String)
+    });
+  }, 10000);
+
+  
+
+});
