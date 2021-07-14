4d26e106aebc8a3896f7e8844fcefdb287c5af53
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-14 12:05:09 -0700

    created new city function

--- __tests__/discord-utils.test.js
+++ __tests__/discord-utils.test.js
@@ -1,7 +1,17 @@
-import { search } from '../lib/discord/discord-utils.js';
+import { search, city } from '../lib/discord/discord-utils.js';
 
 describe('discord utils tests', () => {
   
+  it('takes in a discord command and outputs the city', () => {
+    const input1 = '!scrape chair -c portland';
+    const output = city(input1);
+    expect(output).toEqual('portland');
+
+    const input2 = '!scrape red chair -c seattle';
+    const output2 = city(input2);
+    expect(output2).toEqual('seattle');
+  });
+
   it('takes in a discord command and outputs the search term', () => {
     const input1 = '!scrape chair';
     const output = search(input1);
