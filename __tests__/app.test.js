3c03897dcd971fb223b4b33b090617426f79a85d
Chase <cabbott93@gmail.com>
2021-07-14 11:21:55 -0700

    create results table, failing post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,9 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe.skip('Route tests', () => {
+describe('Route tests', () => {
   const agent = request.agent(app);
-
+  let items;
   beforeAll(async () => {
     await setup(pool);
     const user = {
@@ -22,12 +22,11 @@ describe.skip('Route tests', () => {
 
     const city = 'Portland';
     const searchTerm = 'couch';
-    const items = await request(app)
+    items = await request(app)
       .get(`/api/v1/results/${searchTerm}/${city}`);
 
 
     expect(items.body.length).toBe(132);
-    console.log(items.body.length);
     expect(items.body[0]).toEqual({
       title: expect.any(String),
       image: expect.any(String),
@@ -36,4 +35,12 @@ describe.skip('Route tests', () => {
     });
   }, 14000);
 
+  it('stores results from a users search via POST', async () => {
+    
+    const { body } = await agent
+      .post('/api/v1/results')
+      .send(items.body);
+
+    expect(body).toEqual('');
+  });
 });
